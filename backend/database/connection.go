8e9249b90da6603fdda5a93ec19acd97c561d64d
fujisawaryohei <ryoheifujjiawa@gmail.com>
2022-04-03 23:24:58 +0900

    monorepo用 Docker-compose 追加

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -1,13 +1,27 @@
 package database
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewConnection() (*gorm.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		panic("ENV File not Found")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPass + " dbname=" + dbName + " port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=localhost user=postgres dbname=blog_server port=5432 sslmode=disable",
+		DSN:                  dsn,
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 
